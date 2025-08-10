872a59d561f536f89a4c3aa0047e951ed95dd771
Eric Marcus <eric.marcus@kaiko.ai>
2025-08-10 19:38:19 +0200

    WIP refs

--- vllm/version.py
+++ vllm/version.py
@@ -1,16 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
-try:
-    from ._version import __version__, __version_tuple__
-except Exception as e:
-    import warnings
-
-    warnings.warn(f"Failed to read commit hash:\n{e}",
-                  RuntimeWarning,
-                  stacklevel=2)
-
-    __version__ = "dev"
-    __version_tuple__ = (0, 0, __version__)
+__version__ = "0.8.5.post1+fix"
+__version_tuple__ = (0, 8, 5)
 
 
 def _prev_minor_version_was(version_str):
